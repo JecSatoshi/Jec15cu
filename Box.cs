75c6cfff0567567d1d1748f3bfd0c89a99fe8852
Isono <damedesu3@gmail.com>
2016-12-18 19:37:17 +0900

    はじめてのコミット

---
+++ Box.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour {
+
+    public GameObject original;
+    public GameObject parent;
+    private GameObject a;
+    public GameObject[,] box;
+    public int hen;
+
+	// Use this for initialization
+	void Start () {
+        a = Instantiate(parent, new Vector3((float)hen / 2f, (float)hen / 2f, (float)hen / 2f), Quaternion.identity) as GameObject;
+        box = new GameObject[hen, hen];
+        for (int p = 0; p < hen; p++)   //縦
+        {
+            for (int i = 0; i < hen; i++)   //奥行き
+            {
+                for (int j = 0; j < hen; j++)   //横
+                {
+                    if (p == 0 || p == hen - 1) Instans(j, p, i);
+                    else if (i == 0 || i == hen - 1) Instans(j, p, i);
+                    else if (j == 0 || j == hen - 1) Instans(j, p, i);
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Instans(float j, float p, float i)
+    {
+        GameObject g = Instantiate(original, new Vector3((float)j, (float)p, (float)i), Quaternion.identity) as GameObject;
+        g.transform.parent = a.transform;
+    }
+}
